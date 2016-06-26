dc4a159526490ec241e41614b70fe4c9f36b76e7
Joqlepecheur <joqlepecheur@gmail.com>
2016-06-26 18:36:20 +0200

    add input test

--- gtn.py
+++ gtn.py
@@ -2,23 +2,32 @@
 
 import random
 
-print('Try to guess a number between 1 and 20')
-
 #variables initialization
 
-thenumber = random.randint(1,20)
+rangel = 1 #low boundary
+rangeh = 20 #high boundary
+thenumber = random.randint(rangel,rangeh)
 guess = 0
 tries = 0
 
 #guessing loop
 
+print("Try to guess a number between " + str(rangel) + " and " + str(rangeh))
+
 while guess != thenumber:
-	print('Type your guess:')
-	guess = int(input())
-	tries += 1
-	if guess > thenumber:
-		print('too high')
-	elif guess < thenumber:
-		print('too low')
-	elif guess == thenumber:
-		print ('you win in ' + str(tries) + ' tries!')
+	print("Type your guess:")
+	guess = input()
+	if guess.isdigit():
+		guess = int(guess)
+		if guess < rangel or guess > rangeh :
+			print("Can you count properly?")
+		else:
+			tries += 1
+			if guess > thenumber:
+				print("too high")
+			elif guess < thenumber:
+				print("too low")
+			elif guess == thenumber:
+				print ("you win in " + str(tries) + " tries!")
+	else:
+		print("You know what integers are?")
